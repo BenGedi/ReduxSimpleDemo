a08079e8582dc66e99d10ce5df814a4da827219f
Ben Gedi <bengedi@gmail.com>
2016-06-27 15:58:45 +0300

    add video detail, add initial selectedVideo

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const VideoListItem = ({ video }) => {
   // const video = props.video;
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
   return (
       <li className="list-group-item">
           <div className="video-list media">
               <div className="media-left">
-                  <img className="media-object" />
+                  <img className="media-object" src={imageUrl} />
               </div>
               <div className="media-body" >
-                  <div className="media-heading"></div>
+                  <div className="media-heading">{videoTitle}</div>
               </div>
           </div>
       </li>);
