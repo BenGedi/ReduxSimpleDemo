b204b9ae1d5f04b968421a009a97740e8bb0d32e
Ben Gedi <bengedi@gmail.com>
2016-07-08 01:36:54 +0300

    add search onchange debouns

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -7,7 +7,7 @@ const VideoDetails = ({ video }) => {
     const url = `http://www.youtube.com/embed/${videoId}`;
 
     return (
-        <div className="video-details col-md-8">
+        <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responsive-item" src={ url }></iframe>
             </div>
--- src/index.js
+++ src/index.js
@@ -31,11 +31,11 @@ class App extends Component {
 	}
 
 	render() {
-		const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
 
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={ videoSearch } />
                 <VideoDetail video={ this.state.selectedVideo } />
                 <VideoList
                         onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
