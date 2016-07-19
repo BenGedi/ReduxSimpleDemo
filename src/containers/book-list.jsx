cbe8b0413d5429986e569a8155998d027f32298a
Ben Gedi <bengedi@gmail.com>
2016-07-19 12:18:55 +0300

    minor updates

--- src/containers/book-list.jsx
+++ src/containers/book-list.jsx
@@ -4,26 +4,26 @@ import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
-  render() {
-    return (
-      <ul className="list-group col-sm-4">
-        {this.renderList()}
-      </ul>
-    );
-  }
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
+            );
+        });
+    }
 
-  renderList() {
-    return this.props.books.map((book) => {
-      return (
-        <li
-          key={book.title}
-          onClick={() => this.props.selectBook(book)}
-          className="list-group-item">
-            {book.title}
-        </li>
-      );
-    });
-  }
+    render() {
+        return (
+          <ul className="list-group col-sm-4">
+            {this.renderList()}
+          </ul>
+        );
+    }
+
 }
 
 function mapStateToProps(state) {
